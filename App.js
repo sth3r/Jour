9cb60f4155f961eb38fe964318e65e6149927b40
sth3r <vrodrigues.esther@gmail.com>
2024-09-01 19:29:04 -0300

    initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// import { Container } from './styles';
+
+const Jour = () => {
+  return (
+  <View>
+    <Text>oi</Text>
+  </View>
+  )
+}
+
+export default Jour;
